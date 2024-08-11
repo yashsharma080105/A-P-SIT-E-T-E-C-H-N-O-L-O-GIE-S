a5657f329006bdbe58ab4f56507b02a9c06ac58f
Yash Kumar Sharma <161055465+yashsharma080105@users.noreply.github.com>
2024-08-12 01:19:35 +0530

    Create Simple Calculator.py

---
+++ Simple Calculator.py
@@ -0,0 +1,45 @@
+def multiply(a,b):
+    return a * b
+def substract(a,b):
+    return a - b
+def add(a,b):
+    return a + b
+def divide(a, b):
+    return a % b
+
+
+print("Select the operation : ")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter Choice (1/2/3/4)")
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", substract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+     next_calculation = input("Let's do next calculation? (yes/no): ")
+
+            if next_calculation == "no":
+              break
+        else:
+            print("Invalid Input")
